d52f2ce8a5334c823818496a16f81e5dbd2e027e
pde-rent <paul@astrolab.fi>
2023-11-03 02:26:28 +0100

    exporting tenderly hardhat+ethers for compatibility purpose

--- index.ts
+++ index.ts
@@ -2,3 +2,13 @@ export * from "./types";
 export * from "./networks.ts";
 export * from "./hardhat.config";
 export * from "./utils";
+
+import { ethers, run, network, tenderly } from "hardhat";
+const provider = ethers.provider;
+export {
+    ethers,
+    run,
+    network,
+    provider,
+    tenderly,
+};
