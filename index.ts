35e418e017f315381c109ab6a2452003794100ae
pde-rent <paul@astrolab.fi>
2023-11-11 00:14:37 +0100

    added utils/format export

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 export * from "./types";
 export * from "./networks";
 export * from "./hardhat.config";
+export * from "./utils/fs";
 export * from "./utils/hardhat";
+export * from "./utils/format";
 
 import { ethers, run, network, tenderly } from "hardhat";
 const provider = ethers.provider;
