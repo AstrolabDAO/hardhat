7363f41580ffee332e04a5e3d8dcb953c195fc19
derpa <paul@astrolab.fi>
2024-07-15 16:47:25 +0200

    export multicall objects

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 export * from "./src";
 
 import { ethers, run, network, tenderly } from "hardhat";
+import {
+  Provider as MulticallProvider,
+  Contract as MulticallContract,
+  Call as Multicall,
+  BlockTag as MulticallBlockTag,
+} from "ethcall";
 const provider = ethers.provider;
 export {
-    ethers,
-    run,
-    network,
-    provider,
-    tenderly,
+  ethers,
+  run,
+  network,
+  provider,
+  tenderly,
+  MulticallProvider,
+  MulticallContract,
+  Multicall,
+  MulticallBlockTag,
 };
