a7b586ac582d871786ef03f94b644311f973aab5
derpa <paul@astrolab.fi>
2024-07-17 09:26:57 +0200

    fixed sync fetch for common abis

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+const sfetch = require('sync-fetch');
 
 const [CDN_URL, REGISTRY_URL] = ["https://cdn.astrolab.fi", "https://registry.astrolab.fi"];
 
@@ -8,8 +9,8 @@ const ADDRESSES_URL = `${CDN_URL}/data/addresses-by-chain.json`;
 const REGISTRY_LATEST_URL = `${REGISTRY_URL}/deployments/_latest.json`;
 const CHAINLINK_FEEDS_URL = `${CDN_URL}/data/chainlink-feeds-by-chain.json`;
 const PYTH_FEEDS_URL = `${CDN_URL}/data/pyth-feeds.json`;
-const ERC20_ABI = (require('sync-fetch')(`${REGISTRY_URL}/abis/Erc20.json`)).abi;
-const WETH_ABI = (require('sync-fetch')(`${REGISTRY_URL}/abis/Weth.json`)).abi;
+const ERC20_ABI = sfetch(`${REGISTRY_URL}/abis/Erc20.json`).json()?.abi;
+const WETH_ABI = sfetch(`${REGISTRY_URL}/abis/Weth.json`).json()?.abi;
 const addressZero = "0x0000000000000000000000000000000000000000";
 const addressOne = "0x0000000000000000000000000000000000000001";
 const { MaxUint256, MaxInt256, MinInt256 } = ethers.constants;
